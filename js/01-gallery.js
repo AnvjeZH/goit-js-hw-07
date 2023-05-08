74b44b0d3f4e34820d2e76eb5004768ed9e7e1e4
Anzhelika Zhupynas <121193138+AnvjeZH@users.noreply.github.com>
2023-05-08 18:27:32 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listRef = document.querySelector('.gallery')
+const markup = galleryItems.map(({description, original, preview}) => {
+return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="{description}"
+    />
+  </a>
+</li>
+`
+}).join('')
+
+listRef.insertAdjacentHTML('beforeend', markup)
 
 console.log(galleryItems);
